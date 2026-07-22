82c93fef7764d219c505cde16bb86ee42b6b4668
costafelipe116-gif <costafelipe116@gmail.com>
2026-07-22 19:07:45 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from playwright.sync_api import sync_playwright
+import telebot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(BOT_TOKEN)
+
+def rodar_automacao():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True,
+            executable_path="/usr/bin/chromium"
+        )
+        page = browser.new_page()
+        
+        url_roleta = "https://skylinewgrows.evo-games.com/frontend/evo/r2/#category=all_games&game=roulette&table_id=7x0b1tgh7agmf6hv&lobby_launch_id=2471e41bc5c049ba9c6b4344776e7e98"
+        
+        print(f"Acessando a roleta: {url_roleta}")
+        page.goto(url_roleta)
+        page.wait_for_timeout(10000)
+        
+        print("Página da roleta aberta com sucesso!")
+        browser.close()
+
+@bot.message_handler(commands=['start', 'help'])
+def enviar_boas_vindas(message):
+    bot.reply_to(message, "Opa! Bot rodando perfeitamente no Railway 🚀")
+
+@bot.message_handler(commands=['rodar'])
+def trigger_bot(message):
+    bot.reply_to(message, "Iniciando automação...")
+    try:
+        rodar_automacao()
+        bot.reply_to(message, "Automação finalizada com sucesso! Roleta acessada.")
+    except Exception as e:
+        bot.reply_to(message, f"Erro ao executar: {e}")
+
+if __name__ == "__main__":
+    print("Bot iniciado...")
+    bot.infinity_polling()
